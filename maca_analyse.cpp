dbfa4e9627bdb2c1054b8105bf54bbbbd9d22625
dimazhylko <zhylko.dima@gmail.com>
2019-08-03 19:22:57 +0200

    error handling

--- maca_analyse.cpp
+++ maca_analyse.cpp
@@ -3,13 +3,12 @@
 std::string maca_analyse(std::string config, std::string toki_config_path, std::string input_text) {
 	std::string input_format = "txt", output_format = "plain";
 
-	#TODO: test with custom config 
+	//TODO: test with custom config 
 	if (!toki_config_path.empty()) {
 		Toki::Path::Instance().set_search_path(toki_config_path);
     }
 
 	if (!config.empty()) {
-		try {
 			static boost::shared_ptr<Maca::SentenceAnalyser> sa = Maca::SentenceAnalyser::create_from_named_config(config);
 
 			static std::stringstream out_stream;
@@ -27,18 +26,7 @@ std::string maca_analyse(std::string config, std::string toki_config_path, std::
 			std::string res = out_stream.str();
 			out_stream.str("");
 			return res;
-		} catch (Maca::MacaError& e) {
-			std::cerr << "Maca Error: " << e.info() << "\n";
-			exit(4);
-		} catch (Toki::TokiError& e) {
-			std::cerr << "Tokenizer Error: " << e.info() << "\n";
-			exit(6);
-		} catch (Corpus2::Corpus2Error& e) {
-			std::cerr << "Corpus2 Error: " << e.info() << "\n";
-			exit(8);
-		}
 	} else {
-		std::cerr << "Specify config\n";
-		exit(1);
+		throw std::invalid_argument("Specify maca config");
 	}
 }
--- maca_analyse.i
+++ maca_analyse.i
@@ -3,6 +3,24 @@
         #include "maca_analyse.h"
 %}
 
+%exception {
+        try{
+                $action
+        } catch (Maca::MacaError& e){
+                PyErr_SetString(PyExc_RuntimeError, e.info().c_str());
+                SWIG_fail;
+        } catch (Toki::TokiError& e){
+                PyErr_SetString(PyExc_RuntimeError, e.info().c_str());
+                SWIG_fail;
+        } catch (Corpus2::Corpus2Error& e){
+                PyErr_SetString(PyExc_RuntimeError, e.info().c_str());
+                SWIG_fail;        
+        } catch (std::invalid_argument& e){
+                PyErr_SetString(PyExc_RuntimeError, e.what());
+                SWIG_fail;
+        }
+}
+
 %include "std_string.i"
 %include "maca_analyse.h"
 
