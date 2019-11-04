44867980b8921510e2057180ac413a6da5c4fc6e
dimazhylko <zhylko.dima@gmail.com>
2019-11-04 02:44:05 +0100

    [issue fix] add linewise parameter

--- maca_analyse.cpp
+++ maca_analyse.cpp
@@ -1,6 +1,6 @@
 #include "maca_analyse.h"
 
-std::string maca_analyse(std::string config, std::string toki_config_path, std::string input_text) {
+std::string maca_analyse(std::string config, std::string toki_config_path, std::string input_text, bool linewise, bool split_chunks) {
 	std::string input_format = "txt", output_format = "plain";
 
 	//TODO: test with custom config 
@@ -15,14 +15,29 @@ std::string maca_analyse(std::string config, std::string toki_config_path, std::
 			static boost::shared_ptr<Corpus2::TokenWriter> writer = Corpus2::TokenWriter::create_stream_writer(output_format, out_stream, sa->tagset());
 
 			std::stringstream input_stream(input_text);
-			std::string line;
+			
+			if(linewise){
+				std::string line;
+				while (std::getline(input_stream, line)) {
+					sa->set_input_source(UnicodeString::fromUTF8(line));
+					while (Corpus2::Sentence::Ptr sentence = sa->get_next_sentence()) {
+						writer->write_sentence(*sentence);
+					}
+				}
+			} else {
+				static boost::shared_ptr<Corpus2::TokenReader> tr = boost::make_shared<Maca::TextReader>(input_stream, sa, 1);
 
-			while (std::getline(input_stream, line)) {
-				sa->set_input_source(UnicodeString::fromUTF8(line));
-				while (Corpus2::Sentence::Ptr sentence = sa->get_next_sentence()) {
-					writer->write_sentence(*sentence);
+				if (split_chunks) {
+					while (boost::shared_ptr<Corpus2::Chunk> chunk = tr->get_next_chunk()) {
+						if (!chunk->empty())writer->write_chunk(*chunk);
+					}
+				} else {
+					while (Corpus2::Sentence::Ptr sentence = tr->get_next_sentence()) {
+						writer->write_sentence(*sentence);
+					}
 				}
 			}
+
 			std::string res = out_stream.str();
 			out_stream.str("");
 			return res;
--- maca_analyse.h
+++ maca_analyse.h
@@ -1,10 +1,11 @@
-#include <libcorpus2/io/writer.h>
+#include <libcorpus2/io/writer.h> 
 #include <libmaca/util/sentenceanalyser.h>
 #include <libtoki/util/settings.h>
+#include <libmaca/io/text.h>
 
 #include <boost/algorithm/string.hpp>
 #include <boost/make_shared.hpp>
 
 #include <fstream>
 
-std::string maca_analyse(std::string config, std::string toki_config, std::string input_text);
+std::string maca_analyse(std::string config, std::string toki_config, std::string input_text, bool linewise, bool split_chunks);
